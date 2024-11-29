360ee9e8b107a2ac88ebbf5022c4d4472e035cd0
akira <akira@drox.com>
2024-11-29 10:17:27 +0000

    add fibonacci series function to testing.js

--- testing.js
+++ testing.js
@@ -1,1 +1,10 @@
-// testing.js
+// Function to write the Fibonacci series up to 10
+function fibonacciSeries() {
+    const series = [0, 1];
+    for (let i = 2; i < 10; i++) {
+        series[i] = series[i - 1] + series[i - 2];
+    }
+    return series;
+}
+
+console.log(fibonacciSeries());
